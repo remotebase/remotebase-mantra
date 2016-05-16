dc4bb17a360c5d1cce29e12886648af99bd2fe31
Sung Won Cho <mikeswcho@gmail.com>
2016-05-16 15:31:05 +1000

    Optimize logo in mobile

--- app/modules/core/components/header.jsx
+++ app/modules/core/components/header.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Header = () => (
   <nav className="navbar navbar-light header">
     <div className="container">
-      <a className="navbar-brand" href="/">
-        <img src="images/logo.png" className="logo" alt="logo"/>
-        <span className="name">RemoteBase</span>
-      </a>
-      <ul className="nav navbar-nav pull-xs-right">
+      <div className="navbar-brand-container">
+        <a className="navbar-brand" href="/">
+          <img src="images/logo.png" className="logo" alt="logo"/>
+          <span className="name">RemoteBase</span>
+        </a>
+      </div>
+      <ul className="nav navbar-nav hidden-sm-down">
         <li className="nav-item">
           <a className="nav-link typeform-share" href="https://mike706.typeform.com/to/o6eSiQ" data-mode="1" target="_blank">Add company</a>
         </li>
