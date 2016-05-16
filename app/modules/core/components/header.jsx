ac24c98da8b6b3e8c0949254a7e234832b5920aa
Sung Won Cho <mikeswcho@gmail.com>
2016-05-16 15:22:38 +1000

    Add typeform for adding company

--- app/modules/core/components/header.jsx
+++ app/modules/core/components/header.jsx
@@ -7,6 +7,11 @@ const Header = () => (
         <img src="images/logo.png" className="logo" alt="logo"/>
         <span className="name">RemoteBase</span>
       </a>
+      <ul className="nav navbar-nav pull-xs-right">
+        <li className="nav-item">
+          <a className="nav-link typeform-share" href="https://mike706.typeform.com/to/o6eSiQ" data-mode="1" target="_blank">Add company</a>
+        </li>
+      </ul>
     </div>
   </nav>
 );
