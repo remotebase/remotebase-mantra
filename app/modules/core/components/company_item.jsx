61b863cbc3880fc7f9b83c174e283fbf48e4ca4c
Sung Won Cho <mikeswcho@gmail.com>
2016-05-16 11:23:47 +1000

    Fit 3 cards in a row

--- app/modules/core/components/company_item.jsx
+++ app/modules/core/components/company_item.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
 const CompanyItem = ({company, navToCompany}) => (
-  <li className="company-item-container col-xs-12 col-sm-6" onClick={navToCompany.bind(this, company.slug)}>
+  <li className="company-item-container col-xs-12 col-sm-4" onClick={navToCompany.bind(this, company.slug)}>
     <div className="company-item">
-      <div className="row">
-        <div className="col-xs-12 col-sm-3">
+      <div className="row company-heading">
+        <div className="col-xs-12 text-xs-center">
           <img src={company.getLogoUrl()} alt="company" className="company-logo"/>
         </div>
-        <div className="col-xs-12 col-sm-9">
+        <div className="col-xs-12 text-xs-center">
           <h2 className="company-name">
             {company.name}
           </h2>
-          <p>
-            {company.description}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12 text-xs-center">
+          <p className="short-desc">
+            {company.short_description}
           </p>
         </div>
       </div>
     </div>
+
     <div className="hover-info-container container">
       <div className="hover-info">
         <div className="row">
@@ -24,18 +29,24 @@ const CompanyItem = ({company, navToCompany}) => (
             <h2 className="company-name">
               {company.name}
             </h2>
-            <div className="badges">
-              <span className="rb-label">
-                <i className="fa fa-flag"> </i>{company.founded_year}
-              </span>
-              <span className="rb-label">
-                <i className="fa fa-users"> </i>{company.team_size}
-              </span>
-              {company.fully_distributed ? <span className="rb-label">Fully Distributed</span> : <span></span>}
-            </div>
+            <ul className="list-unstyled summary-list">
+              <li>
+                <i className="fa fa-flag fa-fw"></i> Founded: {company.founded_year}
+              </li>
+              <li>
+                <i className="fa fa-users fa-fw"></i> Team size: {company.team_size}
+              </li>
+              <li>
+                <i className="fa fa-globe fa-fw"></i> Fully Distributed: {company.fully_distributed ? 'YES' : 'NO'}
+              </li>
+            </ul>
+
+
+
             <div className="salary-range">
               {company.salary_lower_bound} ~ {company.salary_upper_bound}
             </div>
+
           </div>
         </div>
       </div>
