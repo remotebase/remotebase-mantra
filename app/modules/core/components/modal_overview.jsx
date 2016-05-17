5371eee98ff9dc1260dac39dbbac8ef35da7d674
Sung Won Cho <mikeswcho@gmail.com>
2016-05-17 10:17:53 +1000

    Implement trait listing

--- app/modules/core/components/modal_overview.jsx
+++ app/modules/core/components/modal_overview.jsx
@@ -1,57 +1,67 @@
 import React from 'react';
 import classnames from 'classnames';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
+
 
 const ModalOverview = ({company, isActive}) => (
   <div className={classnames('tab', 'tab-overview', {active: isActive})}>
     <div className="row">
       <div className="col-xs-12 col-sm-6">
-        <ul className="list-unstyled">
+        <ul className="list-unstyled overview-list">
           <li className="trait-item">
-            <span className="item-name">Founded:</span>
+            <i className="fa fa-flag fa-fw"></i>
+            <span className="item-name">
+              Founded:
+            </span>
             <span className="item-value">{company.founded_year}</span>
           </li>
           <li className="trait-item">
-            <span className="item-name">Fully distributed:</span>
+
+            <i className="fa fa-globe fa-fw"></i>
+            <span className="item-name">
+              Fully distributed
+              <OverlayTrigger
+                overlay={<Tooltip>Does everyone work remotely?</Tooltip>} placement="bottom">
+                <span className="tooltip-trigger">[?]</span>
+              </OverlayTrigger>
+              :
+            </span>
             <span className="item-value">{company.fully_distributed ? 'Yes' : 'No'}</span>
           </li>
           <li className="trait-item">
-            <span className="item-name">Agency:</span>
+            <i className="fa fa-building fa-fw"></i>
+            <span className="item-name">
+              Agency:</span>
             <span className="item-value">{company.is_agency ? 'Yes' : 'No'}</span>
           </li>
           <li className="trait-item">
-            <span className="item-name">Team size:</span>
+            <i className="fa fa-users fa-fw"></i>
+            <span className="item-name">
+              Team size:
+            </span>
             <span className="item-value">{company.team_size}</span>
           </li>
           <li className="trait-item">
-            <span className="item-name">Salary range:</span>
+            <i className="fa fa-money fa-fw"></i>
+            <span className="item-name">
+              Salary range:
+            </span>
             <span className="item-value">
               {company.salary_lower_bound} ~ {company.salary_upper_bound}
             </span>
           </li>
           <li className="trait-item">
-            <span className="item-name">Retreats per year:</span>
-            <span className="item-value">{company.num_retreats_per_year}</span>
-          </li>
-          <li className="trait-item">
-            <span className="item-name">Benefits:</span>
+            <i className="fa fa-map fa-fw"></i>
+            <span className="item-name">
+              Location based salary?
+              <OverlayTrigger
+                overlay={<Tooltip>Do they offer salary adjusted for your cost of living?</Tooltip>} placement="bottom">
+                <span className="tooltip-trigger">[?]</span>
+              </OverlayTrigger>
+              :
+            </span>
             <span className="item-value">
-              {company.healthcare ?
-                <span className="rb-label rb-label-hoverable">
-                  healthcare
-                </span> :
-              ''}
-              {company.family_leave ?
-                <span className="rb-label rb-label-hoverable">
-                  Family leave
-                </span> : ''}
-              {company.unlimited_vacation ?
-                <span className="rb-label rb-label-hoverable">
-                  Unlimited vacation
-                </span> : ''}
-              {company.funded_vacation ?
-                <span className="rb-label rb-label-hoverable">
-                  Funded vacation
-                </span> : ''}
+              {company.location_based_salary ? 'Yes' : 'No'}
             </span>
           </li>
         </ul>
@@ -60,7 +70,52 @@ const ModalOverview = ({company, isActive}) => (
       <div className="col-xs-12 col-sm-6">
         <div className="row">
           <div className="col-xs-12">
-
+            <ul className="list-unstyled overview-list">
+              <li className="trait-item">
+                <span className="item-name">
+                  VC backed?
+                  <OverlayTrigger
+                    overlay={<Tooltip>Are they funded by venture capitals?</Tooltip>} placement="bottom">
+                    <span className="tooltip-trigger">[?]</span>
+                  </OverlayTrigger>
+                  :
+                </span>
+                <span className="item-value">{company.vc_funded ? 'Yes' : 'No'}</span>
+              </li>
+              <li className="trait-item">
+                <span className="item-name">
+                  Retreats per year
+                  <OverlayTrigger
+                    overlay={<Tooltip>How many times do they meet in person per year?</Tooltip>} placement="bottom">
+                    <span className="tooltip-trigger">[?]</span>
+                  </OverlayTrigger>
+                  :
+                </span>
+                <span className="item-value">{company.num_retreats_per_year}</span>
+              </li>
+              <li className="trait-item">
+                <span className="item-name">Benefits:</span>
+                <span className="item-value">
+                  {company.healthcare ?
+                    <span className="rb-label rb-label-hoverable">
+                      healthcare
+                    </span> :
+                  ''}
+                  {company.family_leave ?
+                    <span className="rb-label rb-label-hoverable">
+                      Family leave
+                    </span> : ''}
+                  {company.unlimited_vacation ?
+                    <span className="rb-label rb-label-hoverable">
+                      Unlimited vacation
+                    </span> : ''}
+                  {company.funded_vacation ?
+                    <span className="rb-label rb-label-hoverable">
+                      Funded vacation
+                    </span> : ''}
+                </span>
+              </li>
+            </ul>
           </div>
           <div className="col-xs-12">
 
