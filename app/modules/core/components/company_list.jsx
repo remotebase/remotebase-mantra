c89ded7672ad7fd2593afdc58817e5bbb87baa1c
Sung Won Cho <mikeswcho@gmail.com>
2016-05-20 10:05:56 +1000

    Display newsletter ad

--- app/modules/core/components/company_list.jsx
+++ app/modules/core/components/company_list.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import CompanyItem from '../containers/company_item';
+import NewsletterAd from '../containers/ad_newsletter';
 
-const CompanyList = ({companies}) => (
-  <ul className="list-unstyled row">
-    {
-      companies.map(company => {
-        return <CompanyItem company={company}
-          key={company._id} />;
-      })
-    }
-  </ul>
-);
+const CompanyList = ({companies}) => {
+  let sections = companies;
+
+  if (sections.length > 2) {
+    sections.splice(2, 0, {
+      _adSection: true,
+      component: NewsletterAd
+    });
+  }
+
+  return (
+    <ul className="list-unstyled row">
+      {
+        sections.map((section) => {
+          if (section._adSection) {
+            return <section.component/>;
+          }
+
+          return <CompanyItem company={section}
+            key={section._id} />;
+        })
+      }
+    </ul>
+  );
+}
 
 export default CompanyList;
---
+++ app/modules/core/components/tests/ad_newsletter.js
@@ -0,0 +1,8 @@
+const {describe, it} = global;
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import AdNewsLetter from '../ad_newsletter';
+
+describe('core.components.ad_newsletter', () => {
+  it('should do something');
+});
