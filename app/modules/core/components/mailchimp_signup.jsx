ce7768fa90cf2c8b0a38b6937b78f35b7e975a47
Sung Won Cho <mikeswcho@gmail.com>
2016-05-26 10:33:59 +1000

    drop name

--- app/modules/core/components/mailchimp_signup.jsx
+++ app/modules/core/components/mailchimp_signup.jsx
@@ -27,10 +27,7 @@ class MailchimpSignup extends React.Component {
             <div className="col-xs-12 col-sm-6 cta">
               Get weekly digests of best remote companies and jobs.
             </div>
-            <div className="col-xs-12 col-sm-2">
-              <input type="text" name="NAME" className="form-control" placeholder="Name" required />
-            </div>
-            <div className="col-xs-12 col-sm-2">
+            <div className="col-xs-12 col-sm-3">
               <input type="email" name="EMAIL" className="form-control" placeholder="Email" required />
             </div>
             <div className="col-xs-12 col-sm-2">
