235abd8f5639c23f681d7bb67f06bdd47bb9a8cf
Sung Won Cho <mikeswcho@gmail.com>
2016-05-28 10:14:49 +1000

    Improve description

--- app/modules/core/routes.jsx
+++ app/modules/core/routes.jsx
@@ -10,10 +10,11 @@ export default function (injectDeps, {FlowRouter, _, DocHead, Meteor, Case}) {
   FlowRouter.route('/', {
     name: 'home',
     action() {
-      let title = 'RemoteBase - Best Remote Companies and Remote Jobs';
-      let description = 'RemoteBase is a collection of the best remote companies and remote jobs around the world. Live and work from anywhere.';
+      let title = 'RemoteBase - Find the Best Remote Companies';
+      let description = 'RemoteBase is a collection of the best remote companies around the world. You can live and work from anywhere.';
       DocHead.removeDocHeadAddedTags();
       DocHead.setTitle(title);
+      DocHead.addMeta({name: 'description', content: description});
       DocHead.addMeta({name: 'twitter:card', content: 'summary'});
       DocHead.addMeta({name: 'twitter:site', content: '@remotebase'});
       DocHead.addMeta({name: 'twitter:title', content: title});
@@ -34,14 +35,16 @@ export default function (injectDeps, {FlowRouter, _, DocHead, Meteor, Case}) {
     name: 'company',
     action({companySlug}, {tab}) {
       let companyName = Case.title(companySlug);
-      let title = `RemoteBase - ${companyName} for remote jobs`;
+      let title = `${companyName} for Remote Jobs | RemoteBase`;
+      let description = `See what it's like working remotely at ${companyName}, and check for open positions.`;
       DocHead.removeDocHeadAddedTags();
       DocHead.setTitle(title);
+      DocHead.addMeta({name: 'description', content: description});
       DocHead.addMeta({name: 'twitter:card', content: 'summary'});
       DocHead.addMeta({name: 'twitter:site', content: '@remotebase'});
       DocHead.addMeta({name: 'twitter:title', content: `${companyName} - RemoteBase`});
       DocHead.addMeta({name: 'twitter:image', content: Meteor.absoluteUrl('images/logo.png')});
-      DocHead.addMeta({name: 'twitter:description', content: `How is ${companyName} for a remote job?`});
+      DocHead.addMeta({name: 'twitter:description', content: description});
 
       mount(MainLayoutCtx, {
         content: () => (<Home companySlug={companySlug} companyTab={tab} />)
