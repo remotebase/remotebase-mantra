5eed74c1e751197f7e6563ba2f99d762e245302b
Sung Won Cho <mikeswcho@gmail.com>
2016-06-05 11:12:54 +1000

    Abstract out EnsureLoggedIn component to meteor-auth

--- client/modules/admin/components/dashboard.jsx
+++ client/modules/admin/components/dashboard.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
-import EnsureLoggedIn from '../containers/ensure_logged_in';
+import {EnsureLoggedIn} from 'meteor-auth';
 import DashboardView from '../containers/dashboard_view';
+import Login from '../containers/login';
 
 const Dashboard = ({section}) => (
   <div>
-    <EnsureLoggedIn>
+    <EnsureLoggedIn unauthenticatedMessage={NotLoggedInMessage}>
       <DashboardView section={section} />
     </EnsureLoggedIn>
+  </div>
+);
+
+const NotLoggedInMessage = (
+  <div>
+    <div className="row">
+      <div className="col-xs-12">
+        <div className="alert alert-warning">
+          Please sign in to view this page.
+        </div>
+      </div>
+    </div>
+    <Login />
   </div>
 );
 
