2edbd710b0fb72bb7e23003b73bcec28afa6bf32
Sung Won Cho <mikeswcho@gmail.com>
2016-06-08 18:13:15 +1000

    Fix mobile

--- client/modules/core/components/email_item.jsx
+++ client/modules/core/components/email_item.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const EmailItem = ({email, sendVerificationEmail, removeEmail}) => (
   <li>
     {email.address} - {email.verified ? 'verified' : 'not verified'}
-    <a href="#" onClick={sendVerificationEmail.bind(this, email.address)}>Re-send verification email</a>
+    {
+      email.verified ? <span></span> : <a href="#" onClick={sendVerificationEmail.bind(this, email.address)}>Re-send verification email</a>
+    }
     <a href="#" onClick={removeEmail.bind(this, email.address)}>Remove email</a>
   </li>
 );
