1efe41bdf613019bd4c0f77d38af29bfce995c27
Sung Won Cho <mikeswcho@gmail.com>
2016-06-09 12:18:33 +1000

    fix typo

--- client/modules/core/components/footer.jsx
+++ client/modules/core/components/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
       Hi, I am <a target="_blank" href="https://twitter.com/mikeswcho">@mikeswcho</a> and I made this.
     </p>
     <p>
-      You can add a company <a class="typeform-share link" href="https://mike706.typeform.com/to/o6eSiQ" data-mode="1" target="_blank">Here</a>.
+      You can add a company <a class="typeform-share link" href="https://mike706.typeform.com/to/o6eSiQ" data-mode="1" target="_blank">here</a>.
     </p>
     <p className="mail">
       <a target="_blank" href="https://twitter.com/remotebase">@remotebase</a> | <a href="mailto:hello@remotebase.io">hello@remotebase.io</a>
