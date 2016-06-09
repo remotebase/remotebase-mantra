a90c153cc6ed4ee98a475da78cf85145d6aa7028
Sung Won Cho <mikeswcho@gmail.com>
2016-06-09 14:31:24 +1000

    Fix logo

--- client/modules/admin/components/header.jsx
+++ client/modules/admin/components/header.jsx
@@ -4,8 +4,8 @@ const Header = () => (
   <nav className="navbar navbar-light header">
     <div className="container">
       <div className="navbar-brand-container">
-        <a className="navbar-brand" href="/">
-          <img src="/images/logo.png" className="logo" alt="logo"/>
+        <a className="brand" href="/">
+          <img src="/images/logo.png" className="header-logo" alt="logo"/>
           <span className="name">RemoteBase</span>
         </a>
       </div>
