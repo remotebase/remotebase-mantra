4a4e9bf1be309a64b33c64933cacac0cf3b437f9
Sung Won Cho <mikeswcho@gmail.com>
2016-06-10 10:04:13 +1000

    Add debug insert company method

--- server/methods/debug.js
+++ server/methods/debug.js
@@ -5,37 +5,6 @@ import _ from 'lodash';
 
 export default function () {
   Meteor.methods({
-    'debug.import053016'() {
-      let companies = JSON.parse(Assets.getText('addition-053016.json'));
-      companies.forEach(company => {
-        Companies.insert(company);
-      });
-      console.log('Done debug.import053016');
-    },
-    'debug.undo-import053016'() {
-      let companies = JSON.parse(Assets.getText('addition-053016.json'));
-      companies.forEach(company => {
-        Companies.remove({name: company.name});
-      });
-      console.log('Done debug.undo-import053016');
-    },
-    'debug.import060116'() {
-      let companies = JSON.parse(Assets.getText('addition-060116.json'));
-      companies.forEach(company => {
-        Companies.insert(company);
-      });
-      console.log('Done debug.import060116');
-    },
-    'debug.undo-import060116'() {
-      let companies = JSON.parse(Assets.getText('addition-060116.json'));
-      companies.forEach(company => {
-        Companies.remove({name: company.name});
-      });
-      console.log('Done debug.undo-import053016');
-    }
-  });
-
-  Meteor.methods({
     'debug.getCounts'(column) { // technologies, collaboration_methods, communication_methods
       let companies = Companies.find().fetch();
       let counts = {};
@@ -51,6 +20,10 @@ export default function () {
       });
 
       console.log(counts);
+    },
+
+    'debug.insertCompany'(companyDoc) {
+      Companies.insert(companyDoc);
     }
   });
 }
